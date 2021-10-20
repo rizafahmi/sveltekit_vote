5b3b1626665cf37021193f45a89204a9bb737b65
Riza Fahmi <rizafahmi@gmail.com>
2021-10-20 21:54:47 +0700

    Finish get and update data from sqlite database

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+ const baseUrl = "http://localhost:3000";
+ let topics = [];
+
+ async function getTopics() {
+     const res = await fetch(`${baseUrl}/topics`);
+     const data = await res.json();
+     topics = data.topics;
+ }
+
+ getTopics();
+
+ async function handleVote(id) {
+     console.log("click", id)
+     const res = await fetch(`${baseUrl}/topics`, {
+         method: 'PATCH',
+         body: JSON.stringify({id: id})
+     });
+     const data = await res.json();
+     topics = data.topics;
+ }
+</script>
+
+
+<h1>Vote Topik Livestreaming</h1>
+
+<ul>
+    {#each topics as topic}
+        <li><div>{topic.title} <button on:click={() => handleVote(topic.id)}>Vote({topic.vote})</button></div></li>
+    {/each}
+</ul>
---
+++ src/routes/topics/index.js
@@ -0,0 +1,35 @@
+import betterSqlite from 'better-sqlite3';
+
+const db = betterSqlite('topics.db', {});
+
+db.exec(`CREATE TABLE IF NOT EXISTS topics (
+  id INTEGER PRIMARY KEY ASC,
+  title TEXT,
+  vote INTEGER DEFAULT 0
+)`);
+
+export async function get() {
+	const stmt = db.prepare('SELECT * FROM topics ORDER BY vote DESC');
+	const topics = stmt.all();
+	return {
+		body: {
+			topics
+		}
+	};
+}
+
+export async function patch({ body }) {
+	console.log('PATCH');
+	const data = JSON.parse(body);
+	const stmt = db.prepare(`UPDATE topics SET vote=vote+1 WHERE id=${data.id}`);
+	stmt.run();
+
+	const topicsTable = await get();
+
+	return {
+		body: {
+			status: 'OK',
+			topics: topicsTable.body.topics
+		}
+	};
+}
