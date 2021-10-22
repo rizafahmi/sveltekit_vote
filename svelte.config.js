ad16d35ae97d7712c81f58c231b62170dd649071
Riza Fahmi <rizafahmi@gmail.com>
2021-10-22 21:07:15 +0700

    Add adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 // import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
+import node from '@sveltejs/adapter-node';
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: node()
 	},
 
 	preprocess: [
