251028bd4499a9500376f520211f632604a22e65
Riza Fahmi <rizafahmi@gmail.com>
2021-10-25 09:43:04 +0700

    Change Base url

--- src/routes/overlay.svelte
+++ src/routes/overlay.svelte
@@ -1,7 +1,8 @@
 <script type="text/javascript">
  import { onMount } from 'svelte';
  let topics = [];
- const baseUrl = `http://192.168.1.105:3000`
+ // const baseUrl = `http://192.168.1.105:3000`
+ const baseUrl = "https://fierce-anchorage-54347.herokuapp.com";
 
  async function getTopics() {
      const res = await fetch(`${baseUrl}/topics`);
