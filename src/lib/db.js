5cefecd3a2e75d9c713ccf21f8a0c0e0660fff99
Riza Fahmi <rizafahmi@gmail.com>
2021-10-26 16:55:15 +0700

    Add db url

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,5 +1,7 @@
 import pgp from 'pg-promise';
-const db_url = process.env.DATABASE_URL || 'postgres://localhost:5432/live_vote';
+const db_url =
+	'postgres://iiqmyjfgajemby:94fc483d4954bfc83937df67040753025af7ff12ecf135c591255ef90314e31b@ec2-52-204-14-80.compute-1.amazonaws.com:5432/d9ec136g2sd7th' ||
+	'postgres://localhost:5432/live_vote';
 const db = pgp()(db_url);
 
 export { pgp, db };
