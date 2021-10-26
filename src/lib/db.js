f4a530e34d3bcab251ba7f375114017562da6298
Riza Fahmi <rizafahmi@gmail.com>
2021-10-26 17:42:40 +0700

    Fix SSL db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,15 @@
 import pgp from 'pg-promise';
 const db_url = process.env['DATABASE_URL'] || 'postgres://localhost:5432/live_vote';
-const db = pgp()(db_url);
-pgp().pg.defaults.ssl = {
-	rejectUnauthorized: false
+let ssl = null;
+if (process.env['NODE_ENV'] === 'development') {
+	ssl = { rejectUnauthorized: false };
+}
+
+const config = {
+	connectionString: db_url,
+	ssl: ssl
 };
+
+const db = pgp()(config);
 
 export { pgp, db };
