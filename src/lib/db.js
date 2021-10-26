50e15415099fe2cec3273adfd82c98acfa6eb246
Riza Fahmi <rizafahmi@gmail.com>
2021-10-26 17:00:01 +0700

    Debug DATABASE_URL

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,6 @@
 import pgp from 'pg-promise';
 const db_url = process.env.DATABASE_URL || 'postgres://localhost:5432/live_vote';
-console.log(process.env);
+console.log(process.env.DATABASE_URL);
 const db = pgp()(db_url);
 
 export { pgp, db };
