2349a63b244a70adae8cf45d50d0a7aaaf642291
Riza Fahmi <rizafahmi@gmail.com>
2021-10-26 17:55:43 +0700

    Update ssl lagi

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,11 @@
 import pgp from 'pg-promise';
-const db_url = `${process.env['DATABASE_URL']}` || 'postgres://localhost:5432/live_vote';
+const db_url = process.env['DATABASE_URL'] || 'postgres://localhost:5432/live_vote';
 let ssl = null;
 const config = {
 	connectionString: db_url,
-	ssl: true
+	ssl: {
+		rejectUnauthorized: false
+	}
 };
 
 const db = pgp()(config);
