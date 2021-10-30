5d49116f318aac32be90bf648774ab9fe2ce09fe
Riza Fahmi <rizafahmi@gmail.com>
2021-10-30 10:02:10 +0700

    update limit 6 -> 10

--- src/routes/topics/index.js
+++ src/routes/topics/index.js
@@ -2,7 +2,7 @@ import { db } from '$lib/db.js';
 
 export async function get() {
 	try {
-		const topics = await db.any('SELECT * FROM topics ORDER BY vote DESC LIMIT 6');
+		const topics = await db.any('SELECT * FROM topics ORDER BY vote DESC LIMIT 10');
 		return {
 			body: {
 				topics
